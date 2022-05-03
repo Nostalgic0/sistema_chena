422e830a53fa2708d529d05d2c108e540fb58d84
nostalgico <hectorhenriquez899@gmail.com>
2022-05-02 20:36:37 -0400

    actualizacion

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -15,6 +15,7 @@ class EmpleadoController extends Controller
     public function index()
     {
         //
+        return view('empleado.index');
     }
 
     /**
@@ -25,6 +26,7 @@ class EmpleadoController extends Controller
     public function create()
     {
         //
+        return view('empleado.create');
     }
 
     /**
