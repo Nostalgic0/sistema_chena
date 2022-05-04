2f9e382881a4196d7dce9a222135ac0eaa1264a3
nostalgico <hectorhenriquez899@gmail.com>
2022-05-03 21:13:13 -0400

    hecho la creacion de del formulario de empleados

--- resources/views/empleado/create.blade.php
+++ resources/views/empleado/create.blade.php
@@ -1,1 +1,29 @@
 formulario de creacion de empleados
+<form action="" method="post" >
+    <label for="Rut">Rut</label>
+    <input type="text" name="Rut" id="Rut">
+    <br>
+
+    <label for="Nombre">Nombre</label>
+    <input type="text" name="Nombre" id="Nombre">
+    <br>
+
+    <label for="ApellidoPaterno">Apellido Paterno</label>
+    <input type="text" name="ApellidoPaterno" id="ApellidoPaterno">
+    <br>
+
+    <label for="ApellidoMaterno">Apellido Materno</label>
+    <input type="text" name="ApellidoMaterno" id="ApellidoMaterno">
+    <br>
+
+    <label for="Patente">Patente</label>
+    <input type="text" name="Patente" id="Patente">
+    <br>
+
+    <label for="NumeroLicencia">Numero de Licencia</label>
+    <input type="text" name="NumeroLicencia" id="NumeroLicencia">
+    <br>
+
+    
+    <input type="submit" name="Enviar" id="Enviar"> 
+</form>
