0d98c6203ce7bd3ef682f3340e8ac8e7f6376c6c
nostalgico <hectorhenriquez899@gmail.com>
2022-05-03 21:44:39 -0400

    Mostrando la lista de empleados

--- resources/views/empleado/index.blade.php
+++ resources/views/empleado/index.blade.php
@@ -1,1 +1,28 @@
 mostrar la lista de empleados
+<table class="table table-dark">
+
+    <thead class="thead-light">
+        <tr>
+            <th>Rut</th>
+            <th>Nombre</th>
+            <th>Apellido Paterno</th>
+            <th>Apellido Materno</th>
+            <th>Patente</th>
+            <th>Numero de Licencia</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($empleados as $empleado)
+        <tr>
+            <td>{{ $empleado->rut }}</td>
+            <td>{{ $empleado->Nombre }}</td>
+            <td>{{ $empleado->ApellidoPaterno }}</td>
+            <td>{{ $empleado->ApellidoMaterno }}</td>
+            <td>{{ $empleado->Patente }}</td>
+            <td>{{ $empleado->NumeroLicencia }}</td>
+            <td>Editar | Borrar</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
