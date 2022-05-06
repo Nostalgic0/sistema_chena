21959d17ee90201cc5db7f7ccd1bd76768c56881
nostalgico <hectorhenriquez899@gmail.com>
2022-05-05 21:46:37 -0400

    agregado de estilo al formulario de creacion y de edicion

--- resources/views/empleado/form.blade.php
+++ resources/views/empleado/form.blade.php
@@ -2,30 +2,46 @@
 
 <h1>{{ $modo }} empleado</h1>
 
-<label for="id">Rut</label>
-    <input type="text" name="id" value="{{ isset($empleado->id)?$empleado->id:'' }}" id="id">
-    <br>
+<div class="form-group">
 
+    <label for="id">Rut</label>
+    <input type="text" class="form-control" name="id" value="{{ isset($empleado->id)?$empleado->id:'' }}" id="id">
+    
+</div>
+<div class="form-group">
     <label for="Nombre">Nombre</label>
-    <input type="text" name="Nombre" value="{{ isset($empleado->Nombre)?$empleado->Nombre:'' }}" id="Nombre">
-    <br>
+    <input type="text" class="form-control" name="Nombre" value="{{ isset($empleado->Nombre)?$empleado->Nombre:'' }}" id="Nombre">
+    
+</div>
+<div class="form-group">
 
     <label for="ApellidoPaterno">Apellido Paterno</label>
-    <input type="text" name="ApellidoPaterno" value="{{ isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno:'' }}" id="ApellidoPaterno">
-    <br>
+    <input type="text" class="form-control" name="ApellidoPaterno" value="{{ isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno:'' }}" id="ApellidoPaterno">
+    
+</div>
+
+<div class="form-group">
 
     <label for="ApellidoMaterno">Apellido Materno</label>
-    <input type="text" name="ApellidoMaterno" value="{{ isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno:'' }}" id="ApellidoMaterno">
-    <br>
+    <input type="text" class="form-control" name="ApellidoMaterno" value="{{ isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno:'' }}" id="ApellidoMaterno">
+    
+</div>
+
+
+<div class="form-group">
 
     <label for="Patente">Patente</label>
-    <input type="text" name="Patente" value="{{ isset($empleado->Patente)?$empleado->Patente:'' }}" id="Patente">
-    <br>
+    <input type="text" class="form-control" name="Patente" value="{{ isset($empleado->Patente)?$empleado->Patente:'' }}" id="Patente">
+    
+</div>
+
+<div class="form-group">
 
     <label for="NumeroLicencia">Numero de Licencia</label>
-    <input type="text" name="NumeroLicencia" value="{{ isset($empleado->NumeroLicencia)?$empleado->NumeroLicencia:'' }}" id="NumeroLicencia">
-    <br>
+    <input type="text" class="form-control" name="NumeroLicencia" value="{{ isset($empleado->NumeroLicencia)?$empleado->NumeroLicencia:'' }}" id="NumeroLicencia">
+    
+</div>
+<br>
+    <input type="submit" class="btn btn-success" value="{{ $modo }} Datos">
 
-    <input type="submit" value="{{ $modo }} Datos">
-
-    <a href="{{ url('empleado/') }}">Regresar</a>
+    <a class="btn btn-primary" href="{{ url('empleado/') }}">Regresar</a>
